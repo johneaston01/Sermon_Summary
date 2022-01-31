8a84f2c89972b84a6c16dfc9fadfa66c64552276
John Easton Annepogu <johneastonannepogu@Johns-MBP.lan>
2022-01-31 00:30:00 -0600

    Nav bar initial commit

---
+++ ui/src/containers/header/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+
+    return(
+        <nav>
+            <h1><a href="#">SermonSummary</a></h1>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+            </ul>
+        </nav>
+
+    );
+
+};
+
+export default Header;
